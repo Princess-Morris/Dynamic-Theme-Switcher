be6b0f3809bcd4ca3427649616c918b3f59c8e98
Princess-Morris <okpataprincessmorris3@gmail.com>
2023-12-27 18:24:11 +0100

    added a readme file

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import styled, { ThemeProvider } from 'styled-components';
+import MoonIcon from './components/MoonIcon';
+import SunIcon from './components/SunIcon';
+import Switch from './components/Switch';
 
 function App() {
+  const [theme, setTheme] = useState("light")
+  const isDarkTheme = theme === "dark"
+
+  const toggleTheme = () => {
+    setTheme(isDarkTheme ? "light" : "dark")
+  }
+
+  const StyledApp = styled.div`
+    min-height: 100vh;
+    text-align: center;
+    padding-top: 10rem;
+    background-color: ${(props) => (props.theme.body)} ;
+  `
+  
+  const Name = styled.h1`
+  margin: 1rem;
+  color: ${(props) => (props.theme.title)}
+  `
+  const Info = styled.p`
+  margin: 1rem;
+  color: ${(props) => (props.theme.subtitle)}
+  `
+
+  const darkTheme = {
+    body: "#1c1c1c",
+    title: '#fff',
+    subtitle: "#b6b6b6"
+  }
+
+  const lightTheme = {
+    body: "#fff",
+    title: "#1c1c1c",
+    subtitle: "#333"
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={isDarkTheme ? darkTheme : lightTheme}>
+    <StyledApp>
+      <SunIcon />
+      <Switch toggleTheme={toggleTheme} isDarkTheme={isDarkTheme} />
+      <MoonIcon />
+      <Name>Princess Morris</Name> 
+      <Info>Username: Pmorris</Info>
+      <Info>Email: p.m@gmail.com</Info>
+    </StyledApp>
+    </ThemeProvider>
   );
 }
 
---
+++ src/components/SunIcon.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { FaSun } from "react-icons/fa";
+
+const SunIcon = () => {
+  return (
+    <div>
+      <FaSun />
+    </div>
+  );
+}
+
+export default SunIcon;
